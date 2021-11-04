22fa1619cb58bc4c0566204417e4224ee1cd0657
Natalia Chaika <nataliia.chaika@ucu.edu.ua>
2021-11-04 22:33:13 +0200

    Target 3

--- target_game.py
+++ target_game.py
@@ -20,3 +20,53 @@ def generate_grid() -> List[List[str]]:
     return grid
 # print(generate_grid())
 
+
+def get_words(f: str, letters: List[str]) -> List[str]:
+    """
+    Reads the file f. Checks the words with rules and returns a list of words.
+    """
+    words = []
+    dict = open(f, 'r')
+    content = dict.read()
+    words = content.split("\n")
+    dict.close()
+    del words[0]
+    del words[0]
+    del words[0]
+    words_best = []
+    for i in range(len(words)):
+        words[i] = words[i].lower()
+        if 3 < len(words[i]) < 10 and letters[4] in words[i]:
+            let = list(words[i])
+            values = []
+            for k in range(len(let)):
+                sig = words[i].count(let[k])
+                values.append(sig)
+            k = 0
+            dictionary1 = {}
+            for u in let:
+                dictionary1[u] = values[k]
+                k += 1
+            # print(dictionary1)
+            val = []
+            for b in range(len(letters)):
+                lette = "".join(letters)
+                fab = lette.count(letters[b])
+                val.append(fab)
+            j = 0
+            dictionary2 = {}
+            for c in letters:
+                dictionary2[c] = val[j]
+                j += 1
+            # print(dictionary2)
+            crak = 0
+            for el in dictionary1:
+                # print(el)
+                if (el in dictionary2) and dictionary1[el] <= dictionary2[el]:
+                    crak += 1
+            if crak == len(dictionary1):
+                    words_best.append(words[i])
+            
+    return words_best
+# print(get_words("en1.txt", ["r", "a", "u", "n", "r", "f", "a", "e", "o"]))
+
