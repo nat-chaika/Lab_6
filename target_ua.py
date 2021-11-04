0aa3d50f2b3a895131a578748a51dacc451911aa
Natalia Chaika <nataliia.chaika@ucu.edu.ua>
2021-11-04 22:40:03 +0200

    Target U 4

--- target_ua.py
+++ target_ua.py
@@ -80,3 +80,35 @@ def get_words(fff, letters):
     return final, len(final)
 print(get_words("base.lst", ['й', 'є', 'ю']))
 
+
+def check_user_words(user_words, language_part, letters, dict_of_words):
+    """
+    (list[srt], str, list[str], list[tuple(str, str)]) -> (list[str], list[str])
+    Check if the user words starts with one of the letter from list
+    and wheather they belong to particular language part
+    Return list of correct user words and list of words that user missed
+    """
+
+    words_letter = []
+    for i in range(len(user_words)):
+        if user_words[i][0] in letters:
+            words_letter.append(user_words[i])
+    correct_dict = []
+    for i in range(len(dict_of_words)):
+        if language_part in dict_of_words[i]:
+            correct_dict.append(dict_of_words[i])
+    horse = []
+    dog = []
+    for i in range(len(words_letter)):
+        for kra in range(len(correct_dict)):
+            if words_letter[i] in correct_dict[kra]:
+                horse.append(words_letter[i])
+    for i in range(len(correct_dict)):
+        if correct_dict[i][0] not in horse:
+            dog.append(correct_dict[i][0])
+    return horse, dog
+
+# check_user_words(["fad", "dad", "ger"], "fds", ["f", "d", "v"], [])
+# print(check_user_words([], "verb", ['ю', 'щ', 'я', 'ц', 'г'], get_words("base.lst", ['ю', 'щ', 'я', 'ц', 'г'])))
+# print(check_user_words(['гаяти', 'гнати', 'ініціалізація', 'узяти', 'щавель'], "verb", ['ю', 'щ', 'я', 'ц', 'г']))
+# print(check_user_words(['бабин', 'битий', 'бичий', 'білий', 'бісів', 'богів', 'божий', 'босий', 'булий', 'булів', 'бурий', 'ласий', 'лисий', 'литий', 'лихий', 'лівий', 'любий', 'лютий', 'усний', 'утлий', 'щирий', 'щучий', 'щучин'], "adjective", ['ф', 'у', 'щ', 'б', 'л'], get_words("base.lst", ['ф', 'у', 'щ', 'б', 'л'])))
